784bbd20ea9fc4f1cbd8df89d3b67636a975613a
Sumanth Poojary <128285327+sumanth639@users.noreply.github.com>
2025-03-25 22:51:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // Enable CORS
 app.use(
   cors({
-    origin: 'https://task-manager-olive-tau.vercel.app/dashboard',
+    origin: 'https://task-manager-olive-tau.vercel.app',
     credentials: true,
   })
 );
